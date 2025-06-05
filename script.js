6e00d53081376bccc883bd61ce28c2242eaa87be
Marcos <marcosgodoi051005@gmail.com>
2025-06-05 10:23:02 -0300

    Primeira Parte

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const botao = document.getElementById('meuBotao');
+
+    botao.addEventListener('click', function(event) {
+        criarCoracao(event);
+    });
+
+    function criarCoracao(event) {
+        const botao = event.currentTarget;
+        const coracao = document.createElement('span');
+        coracao.classList.add('coracao');
+        coracao.innerHTML = '❤️';
+        alert ("Tentou ir em ela primeiro ne gostosa.💕💕💕");
+
+        const x = event.clientX - botao.getBoundingClientRect().left;
+        const y = event.clientY - botao.getBoundingClientRect().top;
+        coracao.style.left = `${x}px`;
+        coracao.style.top = `${y}px`;
+
+        botao.appendChild(coracao);
+
+        coracao.addEventListener('animationend', () => {
+            coracao.remove();
+        });
+    }
+});
+document.addEventListener('DOMContentLoaded', () => {
+  const botao = document.getElementById('botao');
+
+  botao.addEventListener('mouseover', () => {
+    botao.style.opacity = '0';
+    botao.style.pointerEvents = 'none';
+  });
+});
